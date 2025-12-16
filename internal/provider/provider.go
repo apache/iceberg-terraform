a85a8cd75036c668628562ad3ad064dd863b7f47
Alex Stephen <1325798+rambleraptor@users.noreply.github.com>
2025-12-16 10:53:12 -0800

    Terraform boilerplate (#1)
    * boilerplate
    
    * Github Workflow
    
    * more boilerplate
    
    * changes
    
    * PR changes
    
    * PR comments
    
    * readme
    
    * Update README.md
    
    Co-authored-by: Fokko Driesprong <fokko@apache.org>
    
    ---------
    
    Co-authored-by: Fokko Driesprong <fokko@apache.org>

---
+++ internal/provider/provider.go
@@ -0,0 +1,64 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+)
+
+var (
+	_ provider.Provider = &icebergProvider{}
+)
+
+// New is a helper function to simplify provider server and testing implementation.
+func New() provider.Provider {
+	return &icebergProvider{}
+}
+
+// icebergProvider is the provider implementation.
+type icebergProvider struct{}
+
+// Metadata returns the provider type name.
+func (p *icebergProvider) Metadata(_ context.Context, _ provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "iceberg"
+}
+
+// Schema defines the provider-level schema for configuration data.
+func (p *icebergProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Description: "Use OpenTofu to interact with Iceberg REST Catalog instances.",
+	}
+}
+
+// Configure prepares a Iceberg API client for data sources and resources.
+func (p *icebergProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	// Provider schema is empty, so no configuration to retrieve.
+}
+
+// DataSources defines the data sources implemented in the provider.
+func (p *icebergProvider) DataSources(_ context.Context) []func() datasource.DataSource {
+	return nil
+}
+
+// Resources defines the resources implemented in the provider.
+func (p *icebergProvider) Resources(_ context.Context) []func() resource.Resource {
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,37 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/apache/iceberg-terraform/internal/provider"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+)
+
+const ADDRESS = "iceberg.apache.org/terraform"
+
+func main() {
+	err := providerserver.Serve(context.Background(), provider.New, providerserver.ServeOpts{
+		// TODO: This needs to change on release with the published name.
+		Address: ADDRESS,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
